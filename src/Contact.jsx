14bce11938650fa3a5b1af8fc0acacd9f136b9b9
sangavi002 <sangavibabu007@gmail.com>
2024-09-11 21:30:37 +0530

    github pages changes

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -18,19 +18,19 @@ export function Contact() {
             </Text>
             <Box display="flex" flexDirection="column" ml={{base:"0px", md:"auto"}} mr={{base:"0px", md:"auto"}} w={{base:"100%", md:"40%"}} gap="20px" color="black" border="1px solid" bg="white" p="10px" mt="30px">
                 <Box display="flex" >
-                    <Img src={github} alt="github" w="10%"/>
+                    <Img src={github} alt="github" w="8%"/>
                     <a href="https://github.com/Sangavi002" target="_blank" style={{padding:"0px 20px"}}>Sangavi002</a>
                 </Box>
                 <Box display="flex" >
-                    <Img src={linkedin} alt="linked" w="10%"/>
+                    <Img src={linkedin} alt="linked" w="8%"/>
                     <a href="https://www.linkedin.com/in/sangavi-babu-7a74a0148/" target="_blank" style={{padding:"0px 20px"}}>Sangavi002</a>
                 </Box>
                 <Box display="flex" >
-                    <Img src={email} alt="email" w="10%"/>
+                    <Img src={email} alt="email" w="8%"/>
                     <a href="https://github.com/Sangavi002" target="_blank" style={{padding:"0px 20px"}}>sangavibabu007@gmail.com</a>
                 </Box>
                 <Box display="flex" >
-                    <Img src={phone} alt="phone" w="10%"/>
+                    <Img src={phone} alt="phone" w="8%"/>
                     <a href="https://github.com/Sangavi002" target="_blank" style={{padding:"0px 20px"}}>+91 9940320771</a>
                 </Box>
             </Box>
