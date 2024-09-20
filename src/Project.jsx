2178ed98f0cc53e0c7111a1da1fb5ec69070dbe6
sangavi002 <sangavibabu007@gmail.com>
2024-09-20 15:29:58 +0530

    worked on project

--- src/Project.jsx
+++ src/Project.jsx
@@ -28,6 +28,7 @@ export function Project() {
                 imageSrc={lululemon}
                 title="LULULEMON"
                 description="Lululemon, a premium athletic apparel retailer founded in 1998, is based in British Columbia and incorporated in Delaware."
+                view="Desktop website"
                 techStack={[html1, css, js, react, redux, chakra]}
                 liveLink="https://lululemonapp.netlify.app/"
                 githubLink="https://github.com/Sangavi002/Lululemon-clone"
@@ -37,6 +38,7 @@ export function Project() {
                 imageSrc={allegro}
                 title="Allegro"
                 description="Allegro, established in 1999, is a leading Polish e-commerce platform and Europe's largest online shopping destination of European origin."
+                view="Desktop website"
                 techStack={[html1, css, js, react, node, express, mongo]}
                 liveLink="https://allegroclone.netlify.app/"
                 githubLink="https://github.com/Sangavi002/allegro-FE"
@@ -45,11 +47,11 @@ export function Project() {
     );
 }
 
-const ProjectBox = ({ imageSrc, title, description, techStack, liveLink, githubLink }) => (
+const ProjectBox = ({ imageSrc, title, description,view, techStack, liveLink, githubLink }) => (
     <Box 
         display="flex" 
         flexDirection={{ base: "column", md: "column",lg:"column" }} 
-        alignItems="center" 
+        alignContent="center"
         p={{ base: "20px", md: "30px" }} 
         border="1px solid white" 
         m={{ base: "20px", md: "20px 50px" }} 
@@ -61,12 +63,13 @@ const ProjectBox = ({ imageSrc, title, description, techStack, liveLink, githubL
         <Box w={{ base: "80%", md: "50%" }} mb={{ base: "20px", md: "0" }}>
             <Img src={imageSrc} alt={title} w="100%" h={{ base: "200px", md: "250px" }}  />
         </Box>
-        <Box color="#795757" w={{ base: "80%", md: "50%" }} p={{ base: "10px", md: "0px 20px" }}>
-            <Text fontSize={{ base: "24px", md: "30px" }} m={{base:"10px 0px",}}><strong>{title}</strong></Text>
-            <Text fontSize={{ base: "18px", md: "18px" }} mt="-10px">{description}</Text>
+        <Box color="#795757" textAlign="left" w={{ base: "80%", md: "50%" }} p={{ base: "10px", md: "0px 40px" }}>
+            <Text fontSize={{ base: "24px", md: "30px" }} textAlign="center" mb={{base:"10px",}}><strong>{title}</strong></Text>
+            <Text fontSize={{ base: "18px", md: "18px" }} textAlign="center" mt="-10px">{description}</Text>
+            <Text fontSize={{ base: "15px", md: "18px" }} mt="5px"><strong>View: </strong>{view}</Text>
             <Box display="flex" h={{ base: "100px", md: "50px" }}>
-                <Box fontSize={{ base: "15px", md: "17px" }} margin="15px 0px" w={{ base: "30%" }}><strong>Tech Stack:</strong></Box>
-                <Box display={{ base: "grid", md: "flex" }} gridTemplateColumns="repeat(4, 1fr)" h="40px" justifyContent="center" mt="10px" gap="10px">
+                <Box fontSize={{ base: "15px", md: "17px" }} margin="15px 0px" w={{ base: "30%",md:"20%" }}><strong>Tech Stack:</strong></Box>
+                <Box display={{ base: "grid", md: "flex" }} gridTemplateColumns="repeat(4, 1fr)" h="40px" justifyContent="start" mt="10px" gap="10px">
                     {techStack.map((tech, index) => (
                         <Img key={index} src={tech} alt={tech} h="30px" />
                     ))}
